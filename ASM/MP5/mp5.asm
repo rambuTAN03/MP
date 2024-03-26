fa420183c28cc3b8e05bfa629e169277a35d0b00
rambuTAN03 <142664911+rambuTAN03@users.noreply.github.com>
2024-03-26 10:18:20 +0800

    Create mp5.asm

---
+++ ASM/MP5/mp5.asm
@@ -0,0 +1,75 @@
+section .data
+    newline db 10   
+
+section .bss
+    buffer resb 1
+
+section .text
+    global _start
+
+_start:
+    ; Read a character from stdin
+    mov eax, 3          
+    mov ebx, 0           
+    mov ecx, buffer     
+    mov edx, 1         
+    int 0x80        
+
+    
+    cmp byte [buffer], '@'
+    jg possible_uppercase
+
+    cmp byte [buffer], 'A'
+    jl end_program
+
+    cmp byte [buffer], 'z'
+    jg end_program
+
+
+   
+    
+possible_uppercase:
+
+    cmp byte [buffer], 'Z'
+    jg possible_lowercase
+
+
+    add byte [buffer], 100000b  
+
+    cmp byte [buffer], '`'
+    jg end_program
+
+    
+
+
+possible_lowercase:
+
+    cmp byte [buffer], 'a'
+    jl end_program
+    cmp byte [buffer], 'z'
+    jg end_program
+
+    sub byte [buffer], 100000b   
+
+
+end_program:
+
+    mov eax, 4          
+    mov ebx, 1          
+    mov ecx, buffer      
+    mov edx, 1         
+    int 0x80
+
+    mov eax, 4          
+    mov ebx, 1        
+    mov ecx, newline  
+    mov edx, 1  
+    int 0x80
+
+
+
+
+    ; Exit the program
+    mov eax, 1           ; sys_exit system call number
+    xor ebx, ebx         ; exit code 0
+    int 0x80             ; invoke system call
